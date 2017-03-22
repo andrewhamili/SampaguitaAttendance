01d9c15a8557856855b04dd23febe656f08c4f5c
andrewhamili <rockr1521@gmail.com>
2017-03-22 19:25:43 +0800

    UI

---
+++ Form1.Designer.vb
@@ -0,0 +1,147 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtLastName = New System.Windows.Forms.TextBox()
+        Me.txtFirstName = New System.Windows.Forms.TextBox()
+        Me.txtMInitial = New System.Windows.Forms.TextBox()
+        Me.cboxCoyesec = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.btnRegister = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtLastName
+        '
+        Me.txtLastName.Location = New System.Drawing.Point(198, 47)
+        Me.txtLastName.Name = "txtLastName"
+        Me.txtLastName.Size = New System.Drawing.Size(184, 26)
+        Me.txtLastName.TabIndex = 0
+        '
+        'txtFirstName
+        '
+        Me.txtFirstName.Location = New System.Drawing.Point(198, 99)
+        Me.txtFirstName.Name = "txtFirstName"
+        Me.txtFirstName.Size = New System.Drawing.Size(184, 26)
+        Me.txtFirstName.TabIndex = 1
+        '
+        'txtMInitial
+        '
+        Me.txtMInitial.Location = New System.Drawing.Point(198, 141)
+        Me.txtMInitial.Name = "txtMInitial"
+        Me.txtMInitial.Size = New System.Drawing.Size(48, 26)
+        Me.txtMInitial.TabIndex = 2
+        '
+        'cboxCoyesec
+        '
+        Me.cboxCoyesec.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cboxCoyesec.FormattingEnabled = True
+        Me.cboxCoyesec.Items.AddRange(New Object() {"BSBIO-3A", "BSBIO-4A", "BSCPE-4A", "BSCPE-5A", "BSCS-3A", "BSCS-4A", "BSIT-3A", "BSIT-4A", "BSPSY-3A", "BSPSY-3B", "BSPSY-3C", "BSPSY-3D", "BSPSY-4A", "BSPSY-4B", "BSPSY-4C", "BSPSY-4D"})
+        Me.cboxCoyesec.Location = New System.Drawing.Point(198, 190)
+        Me.cboxCoyesec.Name = "cboxCoyesec"
+        Me.cboxCoyesec.Size = New System.Drawing.Size(121, 26)
+        Me.cboxCoyesec.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(34, 50)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(84, 18)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Last Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(34, 102)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(85, 18)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "First Name"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(34, 144)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(94, 18)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Middle Initial"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(34, 193)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(158, 18)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Course, Year, Section"
+        '
+        'btnRegister
+        '
+        Me.btnRegister.ForeColor = System.Drawing.Color.Black
+        Me.btnRegister.Location = New System.Drawing.Point(198, 236)
+        Me.btnRegister.Name = "btnRegister"
+        Me.btnRegister.Size = New System.Drawing.Size(89, 30)
+        Me.btnRegister.TabIndex = 8
+        Me.btnRegister.Text = "Register"
+        Me.btnRegister.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 18.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(51, Byte), Integer), CType(CType(51, Byte), Integer), CType(CType(51, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(527, 293)
+        Me.Controls.Add(Me.btnRegister)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cboxCoyesec)
+        Me.Controls.Add(Me.txtMInitial)
+        Me.Controls.Add(Me.txtFirstName)
+        Me.Controls.Add(Me.txtLastName)
+        Me.Font = New System.Drawing.Font("Arial", 12.0!)
+        Me.ForeColor = System.Drawing.Color.White
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtLastName As System.Windows.Forms.TextBox
+    Friend WithEvents txtFirstName As System.Windows.Forms.TextBox
+    Friend WithEvents txtMInitial As System.Windows.Forms.TextBox
+    Friend WithEvents cboxCoyesec As System.Windows.Forms.ComboBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents btnRegister As System.Windows.Forms.Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+
+    Private Sub btnRegister_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnRegister.Click
+
+    End Sub
+End Class
